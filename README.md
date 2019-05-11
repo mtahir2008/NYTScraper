0161acc6928708118aa41857e7dba6a9d4c4d9ae
mtahir2008 <mohsin.m.tahir@gmail.com>
2019-05-10 19:39:05 -0700

    it's all working

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # NYTScraper
+
+![](./public/assets/images/screenshot.png)
+### Overview
+A web app utilizes below technologies to let users view latest news Buzz!!
+MongoDB
+Mongoose
+Cheerio
+Express
+
+### Technologies Used
+HTML
+CSS Bootstrap
+CSS
+JavaScript
+jQuery
+Node.js
+NPM Express
+NPM Path
---
+++ public/assets/javascript/index.js
@@ -0,0 +1,112 @@
+$(document).ready(function() {
+
+  var articleContainer = $(".article-container");
+  $(document).on("click", ".btn.save", handleArticleSave);
+  $(document).on("click", ".scrape-new", handleArticleScrape);
+  $(".clear").on("click", handleArticleClear);
+
+  function initPage() {
+    // Run an AJAX request for any unsaved headlines
+    $.get("/api/headlines?saved=false").then(function(data) {
+
+      articleContainer.empty();
+      // If we have headlines, render them to the page
+      if (data && data.length) {
+        renderArticles(data);
+      } else {
+        // Otherwise render a message explaining we have no articles
+        renderEmpty();
+      }
+    });
+  }
+
+  function renderArticles(articles) {
+
+    var articleCards = [];
+
+    for (var i = 0; i < articles.length; i++) {
+      articleCards.push(createCard(articles[i]));
+    }
+    articleContainer.append(articleCards);
+  }
+
+  function createCard(article) {
+    // article card
+    var card = $("<div class='card'>");
+    var cardHeader = $("<div class='card-header'>").append(
+      $("<h3>").append(
+        $("<a class='article-link' target='_blank' rel='noopener noreferrer'>")
+          .attr("href", article.url)
+          .text(article.headline),
+        $("<a class='btn btn-success save'>Save Article</a>")
+      )
+    );
+
+    var cardBody = $("<div class='card-body'>").text(article.summary);
+
+    card.append(cardHeader, cardBody);
+    card.data("_id", article._id);
+    // We return the constructed card jQuery element
+    return card;
+  }
+
+  function renderEmpty() {
+    var emptyAlert = $(
+      [
+        "<div class='alert alert-warning text-center'>",
+        "<h4>Uh Oh. Looks like we don't have any new articles.</h4>",
+        "</div>",
+        "<div class='card'>",
+        "<div class='card-header text-center'>",
+        "<h3>What Would You Like To Do?</h3>",
+        "</div>",
+        "<div class='card-body text-center'>",
+        "<h4><a class='scrape-new'>Try Scraping New Articles</a></h4>",
+        "<h4><a href='/saved'>Go to Saved Articles</a></h4>",
+        "</div>",
+        "</div>"
+      ].join("")
+    );
+    // Appending this data to the page
+    articleContainer.append(emptyAlert);
+  }
+
+  function handleArticleSave() {
+    var articleToSave = $(this)
+      .parents(".card")
+      .data();
+
+    // Remove card from page
+    $(this)
+      .parents(".card")
+      .remove();
+
+    articleToSave.saved = true;
+    // Using a patch method to be semantic since this is an update to an existing record in our collection
+    $.ajax({
+      method: "PUT",
+      url: "/api/headlines/" + articleToSave._id,
+      data: articleToSave
+    }).then(function(data) {
+      // If the data was saved successfully
+      if (data) {
+        location.reload();
+      }
+    });
+  }
+
+  function handleArticleScrape() {
+    // This function handles the user clicking any "scrape new article" buttons
+    $.get("/api/fetch").then(function(data) {
+      window.location.href = "/";
+    });
+  }
+
+  function handleArticleClear() {
+    $.get("api/clear").then(function(data) {
+      articleContainer.empty();
+      // initPage();
+      location.reload();
+    });
+  }
+});
